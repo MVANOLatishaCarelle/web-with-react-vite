39268aca51ea7e89cb7ab0cb2cf9f64a9927bcc7
MVANOLatishaCarelle <latishacarellemvano@gmail.com>
2025-05-07 11:36:03 +0200

    Septieme commit

--- src/components/platPage.jsx
+++ src/components/platPage.jsx
@@ -11,26 +11,32 @@ function PlatPage(){
                 <Menu/>
                 <div className="mt-20"></div>
         <div className="p-6 max-w-3xl mx-auto">
-        <h1 className="text-3xl font-bold mb-4">Gestion des Plats</h1>
-        <form className="space-y-4 mb-6">
+        <h1 className="text-3xl font-bold mb-4" style={{ textShadow:'2px 4px 2px sand'}}>Administration Des Plats</h1>
+        <form className="bg-white p-8 rounded-xl shadow-md w-full max-w-md mx-auto space-y-5">
             <input
             type="text"
             placeholder="Nom"
-            className="border px-4 py-2 rounded w-full"
+            className="w-full border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
             required
             />
-            <input
+            <textarea
             type="text"
             placeholder="Description"
-            className="border px-4 py-2 rounded w-full"
+            className="w-full border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 resize-none"
             />
             <input
             type="number"
             placeholder="Prix"
-            className="border px-4 py-2 rounded w-full"
+            className="w-full border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
             required
             />
-            <button type="submit" className="bg-blue-600 text-white px-4 py-2 rounded">
+            <input
+            type="file"
+            placeholder="Photo du plat"
+            className="w-full border border-gray-300 px-4 py-2 rounded-md bg-white file:bg-blue-600 file:text-white file:px-4 file:py-2 file:rounded-md file:border-0 file:cursor-pointer"
+            required
+            />
+            <button type="submit" className="w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 rounded-md transition">
             {isEditing ? "Modifier" : "Créer"}
             </button>
       </form>
